ea648985a14d3aa7680ce83efa3e80e678bad14d
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2018-12-28 15:53:47 +0100

    Fixed proper Back navigation with the back stack

--- app/src/main/java/com/example/hanna/maze/MainActivity.java
+++ app/src/main/java/com/example/hanna/maze/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hanna.maze;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -60,7 +64,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         Intent gameIntent = new Intent(this, GameActivity.class);
         gameIntent.putExtra( "choice", choice);
+
+        PendingIntent pendingIntent = TaskStackBuilder.create(this)
+                .addNextIntentWithParentStack(gameIntent)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        
         startActivity(gameIntent);
-        finish();
     }
 }
